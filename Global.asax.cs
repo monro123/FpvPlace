a4ab9fadf654386e45c26e015029528bc9159095
monro123 <140950161+monro123@users.noreply.github.com>
2023-08-22 07:56:58 -0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace AmitmaizusFPV
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["maxScore"] = 0;
+        }
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["Phone_Number"] = "";
+            Session["Email"] = "";
+            Session["First_Number"] = "";
+            Session["Last_Number"] = "";
+            Session["isSigned"] = false;
+            Session["Admin"] = false;
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Session["Phone_Number"] = "";
+            Session["Email"] = "";
+            Session["First_Number"] = "";
+            Session["Last_Number"] = "";
+            Session["isSigned"] = false;
+            Session["Admin"] = false;
+        }
+    }
+}
---
+++ dbo.contact(4).data.sql
@@ -0,0 +1,13 @@
+﻿INSERT INTO [dbo].[contact] ([Id], [First_Name], [Last_Name], [Password], [Phone_Number], [Email], [Subject], [Message], [Feedback_Rating], [useful_Topic], [Additional_Info_Topic], [Website_Satisfaction]) 
+VALUES 
+   (12, N'amit', N'maizus', N'Koren123', N'0536401923', N'ami@gmail.com', N'informathion', N'where can I found more information about drones?', 4, N'Starting out', N'Building', 5),
+   (18, N'Amy', N'Roberts', N'Passw0rd123', N'555-6789', N'amy.roberts@example.com', N'Feedback', N'I love the FPV drone I purchased! It''s amazing!', 5, N'Product Review', N'Satisfaction', 5),
+   (19, N'John', N'Doe', N'JohnDoe123', N'123-4567', N'john.doe@example.com', N'Question', N'What is the maximum range of an FPV drone?', 3, N'Technical Information', N'Range', 4),
+   (20, N'Sarah', N'Smith', N'SarahSmith789', N'987-6543', N'sarah.smith@example.com', N'Feedback', N'The customer support was very helpful and responsive!', 5, N'Customer Support', N'Satisfaction', 5),
+   (21, N'David', N'Johnson', N'DavidJ123', N'555-1234', N'david.johnson@example.com', N'Question', N'Can FPV drones be flown at night?', 4, N'Regulations', N'Night Flying', 3),
+   (22, N'Emily', N'Wilson', N'EmilyW456', N'222-3333', N'emily.wilson@example.com', N'Feedback', N'I had a great experience with the FPV drone workshop!', 5, N'Workshop', N'Satisfaction', 5),
+   (23, N'Michael', N'Anderson', N'MikeA789', N'444-5678', N'michael.anderson@example.com', N'Question', N'What is the average flight time of an FPV drone?', 4, N'Technical Information', N'Flight Time', 4),
+   (24, N'Olivia', N'Martin', N'OliviaM123', N'999-8888', N'olivia.martin@example.com', N'Feedback', N'The FPV drone meetup was a lot of fun!', 5, N'Meetup', N'Entertainment', 5),
+   (25, N'James', N'Thompson', N'JamesT456', N'777-1111', N'james.thompson@example.com', N'Question', N'What are the recommended safety precautions for flying FPV drones?', 4, N'Safety', N'Precautions', 4),
+   (26, N'Sophia', N'Walker', N'SophiaW789', N'222-4444', N'sophia.walker@example.com', N'Feedback', N'The FPV drone tutorial videos were very informative!', 5, N'Tutorial Videos', N'Information', 5),
+   (27, N'William', N'Clark', N'WilliamC123', N'888-2222', N'william.clark@example.com', N'Question', N'Are FPV drones suitable for aerial photography?', 4, N'Photography', N'Aerial Photography', 3);
